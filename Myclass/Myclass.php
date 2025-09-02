936abe26786a2d9b27b4819fc0988556e52361fc
krish moradiya <140172827+kriskumarmoradiya0501@users.noreply.github.com>
2025-09-02 16:12:23 +0530

    Create Myclass.php

---
+++ Myclass/Myclass.php
@@ -0,0 +1,31 @@
+<?php
+class Myclass {
+    public $number;
+
+    function myfunction() {
+        echo "Hello World";
+    }
+
+    function __construct($number) {
+        $this->number = $number;
+        echo "Constructor called";
+    }
+
+    function sum () {
+        $n = $this->number;
+        $sum1 = 0;
+        for ($i = 1; $i <= $n; $i++) {
+            $term = $i * $i;
+            if ($i % 2 == 0) {
+                echo "+ $term ";
+                $sum1 += $term;
+            } else {
+                echo "- $term ";
+                $sum1 -= $term;
+            }
+        }
+        echo "\nSum of Series 1: $sum1\n";
+        return $sum1;
+    }
+}
+?>
